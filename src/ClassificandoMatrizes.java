1c529e0b8114bf29f2795e6a2a66a176889f6331
llpupp <llpupp@hotmail.com>
2022-12-02 09:34:54 -0300

    Classificando a matriz

--- src/ClassificandoMatrizes.java
+++ src/ClassificandoMatrizes.java
@@ -1,5 +1,37 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+import java.util.*;
+import java.util.Scanner;
+import static java.lang.System.*;
+public class ClassificandoMatrizes {
+
+       public static void main(String[] args) {
+// Mover todos os inteiros pares para o inicio do array, seguido por todos os números ímpares no final do Array
+
+           // Criando o objeto scanner:
+           Scanner scan = new Scanner(in);
+
+           out.println("Digite o tamanho do Array:");
+           int tamanhoArray = scan.nextInt();
+           int [] nums = new int[tamanhoArray];
+           int primeiroDoIndex = 0;
+
+           out.println("Digite os números do Array: ");
+           // Gerando o array inicial:
+           for (int i = 0; i < tamanhoArray; i++) nums[i] = scan.nextInt();
+
+           // Estrutura de Repetição que ordena o array:
+           for(int i = 0; i < tamanhoArray; i++) {
+               if(nums[i] % 2 == 0) {
+                   int varAuxiliar = nums[primeiroDoIndex];
+                   nums[primeiroDoIndex] = nums[i];
+                   nums[i] = varAuxiliar;
+                   primeiroDoIndex++;
+               }
+           }
+
+           // Resultado:
+           for(int numero : nums) out.println(numero);
+       }
 }
+
+
